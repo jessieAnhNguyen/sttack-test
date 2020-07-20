d515f1d55dce2c00e2c72313320a9d99c1664eb0
Jessie Anh Nguyen <phuonganhnguyenthi26.3@gmail.com>
2020-07-20 10:38:04 +0700

    added a simple func

--- index.ts
+++ index.ts
@@ -2,3 +2,9 @@ console.log("hello testing stack-attack");
 console.log("first generating the sttack.config.json");
 
 console.log("then use yarn cli <repoPath>");
+
+function count(number) {
+  console.log(number);
+  number += 1;
+}
+count(1);
